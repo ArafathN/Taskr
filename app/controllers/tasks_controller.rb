9b28fc6157ccdf18bed71c806f151b11b3d3c17b
ANGUYEN <nguyen.arafath@gmail.com>
2020-08-31 21:23:50 +0200

    Created user task association and updated controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
+before_action :authenticate_user!
 before_action :set_task, only:[:edit, :update, :show, :destroy]
 
 	def index
-		@task = Task.all
+		@tasks = current_user.tasks
+
 	end
 
 	def new
@@ -15,7 +17,7 @@ before_action :set_task, only:[:edit, :update, :show, :destroy]
 
 
 	def create
-		@task = Task.new(tasks_params)
+		@task = current_user.tasks.new(tasks_params)
 		if @task.save
 		flash[:notice] = "Task was successfully created"
 		redirect_to task_path(@task)
